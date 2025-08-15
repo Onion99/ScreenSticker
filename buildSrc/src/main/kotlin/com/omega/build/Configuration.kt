cd634eff3cd76358c0a9289c47f54ad36e27a95c
OnionTiger <lijundong@inke.cn>
2025-08-15 10:23:07 +0800

    chore: miss file

---
+++ buildSrc/src/main/kotlin/com/omega/build/Configuration.kt
@@ -0,0 +1,20 @@
+package com.omega.build
+
+object Configuration {
+  const val app = "Snipaste"
+  const val compileSdk = 36
+  const val targetSdk = 36
+  const val minSdk = 26
+  private const val majorVersion = 0
+  private const val minorVersion = 0
+  private const val patchVersion = 1
+  const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+  val versionCode = "$majorVersion$minorVersion$patchVersion".toInt()
+  const val nameSpace= "com.omega.sun"
+  const val debugApplicationId= "com.omega.sun"
+  const val debugSignPassWord = "nova9999"
+  const val debugSignAlias = "nova"
+  const val releaseApplicationId = "com.omega.star"
+  const val releaseSignPassWord = "nova9999"
+  const val releaseSignAlias = "nova"
+}
